cb34f4367a5d1c4140e1dc9323ff65b00e074bbd
xup6zeo <tonyl@ivicon.com>
2016-07-25 10:50:49 -0500

    format 2016/7/25

--- AngularJS OMDb/omdb.js
+++ AngularJS OMDb/omdb.js
@@ -1,19 +1,17 @@
-var myApp = angular.module('app', []);
-//註解1         
+var myApp = angular.module('app', [])
+// 註解1         
 myApp.controller('SearchCtrl', function ($scope, $http) {
-
-    $scope.search = function () {
-        $http({
-
-            method: 'GET',
-            url: "http://www.omdbapi.com/?t=" + $scope.t + "&y=" + $scope.y + "&plot=" + $scope.plot + "&r=" + $scope.r,
-            params: { name: 'omdb' }
-        })
-            .success(function (data) {
-                $scope.result = data;
-                $scope.url = "http://www.omdbapi.com/?t=" + $scope.t + "&y=" + $scope.y + "&plot=" + $scope.plot + "&r=" + $scope.r;
-                $scope.sw = true;
-            });
-    }
+  $scope.search = function () {
+    $http({
+      method: 'GET',
+      url: 'http://www.omdbapi.com/?t=' + $scope.t + '&y=' + $scope.y + '&plot=' + $scope.plot + '&r=' + $scope.r,
+      params: { name: 'omdb' }
+    })
+      .success(function (data) {
+        $scope.result = data
+        $scope.url = 'http://www.omdbapi.com/?t=' + $scope.t + '&y=' + $scope.y + '&plot=' + $scope.plot + '&r=' + $scope.r
+        $scope.sw = true
+      })
+  }
 }
-);
+)
